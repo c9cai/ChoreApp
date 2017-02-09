1372d776562e4da42fbb1c4ada9bdee4248448e8
Brian Chao <brianchao@Brians-MacBook-Air-3.local>
2017-02-09 12:40:27 -0800

    updated home page

--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,17 @@
+var data = require("../json/chore_schedule.json");
+
 exports.viewHome = function(req, res) {
-    res.render('home', {
-    });
+    for (var category in data) {
+        category_data = data[category];
+
+        for (var chore in category_data) {
+            chore_data = category_data[chore];
+            if (chore_data['username'] != "Brian")
+                delete data[category][chore];
+        }
+    }
+
+    console.log(data);
+
+    res.render('home', data);
 };
