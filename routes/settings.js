a5ac4946fe338f875ef9c471ecfa7563645a8fb8
Brian Chao <brianchao@brians-air-3.gateway.pace.com>
2017-02-16 16:40:11 -0800

    updated routes/settings.js

--- routes/settings.js
+++ routes/settings.js
@@ -1,13 +1,14 @@
 var user_data = require("../json/users.json");
+var chore_data = require("../json/chore_schedule.json");
 
 exports.viewSettings = function(req, res) {
     var rendData = {};
     rendData["users"] = [];
-    currentUser = user_data['current_user']['username'];
+    currentUser = chore_data['current_user']['username'];
 
-    for (var user in user_data['users']) {
-    	if (user_data['users'][user]['username'] != currentUser) {
-    		rendData["users"].push(user_data['users'][user]['username']);
+    for (var user in user_data) {
+    	if (user_data[user]['username'] != currentUser) {
+    		rendData["users"].push(user_data[user]['username']);
     	}
     }
     res.render('settings', rendData);
