fc9516d138c80240d65a3d1c3d5f6d319a2cb2ac
Brian Chao <brianchao@Brians-MacBook-Air-3.local>
2017-02-21 14:09:53 -0800

    example json data

--- routes/home.js
+++ routes/home.js
@@ -2,12 +2,15 @@ var chore_data = require("../json/chore_schedule.json");
 var user_data = require("../json/users.json");
 
 exports.viewHome = function(req, res) {
-    var username = chore_data['current_user']['username'];
-    var password = chore_data['current_user']['password'];
-    var rendData = chore_data[username];
-    rendData['rating'] = user_data[username + "_" + password]['rating'];
-    console.log(rendData);
-    res.render('home', rendData);
+    if (chore_data['current_user'] != null) {
+        var username = chore_data['current_user']['username'];
+        var password = chore_data['current_user']['password'];
+        var rendData = chore_data[username];
+        rendData['rating'] = user_data[username + "_" + password]['rating'];
+        res.render('home', rendData);
+    } else {
+        res.render('login');
+    }
 };
 
 exports.jsonHome = function(req, res) {
