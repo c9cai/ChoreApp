39ab14a00fb026cb939c619ecce12ff42c49a0be
Brian Chao <brianchao@Brians-MacBook-Air-3.local>
2017-02-21 16:30:36 -0800

    login/check login done

--- routes/check_login.js
+++ routes/check_login.js
@@ -1,23 +1,34 @@
-var user_data = require("../json/users.json");
-var chore_data = require("../json/chore_schedule.json");
+//firebase
+var firebase = require('firebase').initializeApp({
+    serviceAccount: "./ChoreApp-d6f6f8302527.json",
+    databaseURL: "https://choreapp-ed3c0.firebaseio.com"
+});
+
+var userRef = firebase.database().ref("users");
+
+//local files
+var current_user = require("../json/current_user.json");
 
 exports.checkLogin = function(req, res) {
-    var username = req.body.username;
+    var username = req.body.username.replace(".","");
     var password = req.body.password;
     var check = false;
 
-    //check valid username password
-    if (username != null && password != null) {
+    console.log("check login");
 
-        if (user_data[username + "_" + password] != null) {
-            check = true;
-            chore_data['current_user'] = user_data[username + "_" + password];
-            res.redirect("/home");
-        }
-    } else {
-        res.render('login');
-    }
+    userRef.on("value", function(snapshot) {
+        var user_data = snapshot.val();
 
-    if (!check)
-        res.render('login');
+        if (user_data[username] != null) {
+            if (user_data[username]['password'] == password) {
+                check = true;
+                current_user['current_user'] = user_data[username];
+
+                console.log("login success");
+                res.redirect("/home");
+            } else
+                res.render('login');
+        } else
+            res.render('login');
+    });
 };
--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,8 @@
-var chore_data = require("../json/chore_schedule.json");
+//local files
+var current_user = require("../json/current_user.json");
 
 exports.viewLogin = function(req, res) {
-    if (chore_data['current_user'] == null)
+    if (current_user['current_user'] == null)
         res.render('login');
     else {
         res.redirect("/home");
