a2473b75ba5009dd1c98e2dba0d4e5329c3e1850
Brian Chao <brianchao@brians-air-3.gateway.pace.com>
2017-02-23 23:19:24 -0800

    Fixed settings page

--- routes/settings.js
+++ routes/settings.js
@@ -6,39 +6,50 @@ var firebase = firebaseModule.firebase;
 
 var user_data = [];
 var chore_data = [];
+var homes_data = [];
 
 var ref = firebase.database().ref('users');
-ref.on("value",function(snapshot) {
+ref.on("value", function (snapshot) {
     user_data = snapshot.val();
 });
 
 var ref2 = firebase.database().ref('chores');
-ref2.on('value',function(snapshot) {
+ref2.on('value', function (snapshot) {
     chore_data = snapshot.val();
 });
 
+var home_ref = firebase.database().ref('homes');
+home_ref.on('value', function (snapshot) {
+    homes_data = snapshot.val();
+});
 
-exports.viewSettings = function(req, res) {
-    data = [];
+
+exports.viewSettings = function (req, res) {
+    var data = [];
     var rating;
-    var name; 
+    var name;
 
     if (current_user['current_user'] != null) {
-        
-        for (user in user_data) {
-            //console.log(user);
-          var first = user_data[user]['firstName'];
-          var last = user_data[user]['lastName'];
-          data.push(first + ' ' + last);
-          if (user == current_user['current_user']['email'].replace(".",""))  {
-            //var homeName = user_data[user]['homeName'];
-            //console.log(homeName);
-            rating = user_data[user]['rating'];
-            name = user_data[user]['firstName'];
-          }
+        var homeName = current_user['current_user']['homeName'];
+        var home_data = homes_data[homeName];
+        console.log(home_data);
+        //loop through each user in home
+        for (user in home_data) {
+            var authEmail = home_data[user].split('.').join('');
+
+            //get first and last name of user
+            var first = user_data[authEmail]['firstName'];
+            var last = user_data[authEmail]['lastName'];
+            data.push(first + ' ' + last);
+
+            //set rating and firstname of current user
+            if (authEmail == current_user['current_user']['email'].split('.').join('')) {
+                rating = user_data[authEmail]['rating'];
+                name = user_data[authEmail]['firstName'];
+            }
         }
 
-        
+
         var rendData = {};
         rendData["users"] = data;
         rendData['rating'] = rating;
@@ -47,6 +58,6 @@ exports.viewSettings = function(req, res) {
         res.render('settings', rendData);
     }
     else {
-            res.render('login');
+        res.render('login');
     }
 };
